a7dd812bd510da1e043264e0abba742352eed212
Swarup123-max <swarupdas943711@gmail.com>
2025-03-15 19:01:06 +0530

    version 7 modified

--- example.java
+++ example.java
@@ -2,4 +2,9 @@ public class example{
     public static void main(String[] args) {
         System.out.println("hello world");
     }
+} 
+class waste{
+    static void timepass(){
+        System.out.println("bye world");
+    }
 }
